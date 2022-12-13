e5d757e0ff0ad80597ab025de5b30954b0c003a8
Juan Camilo Camargo <camilocm_67@hotmail.com>
2022-12-12 19:43:58 -0500

    fix: format

--- release.config.js
+++ release.config.js
@@ -5,11 +5,14 @@ module.exports = {
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
         "@semantic-release/npm",
-        ["@semantic-release/github",{
-            assets: [
-                { path: "build.zip",label:"Build"},
-                { path: "coverage.zip",label:"Test coverage"}
-            ]
-        }],
+        [
+            "@semantic-release/github",
+            {
+                assets: [
+                    { path: "build.zip", label: "Build" },
+                    { path: "coverage.zip", label: "Test coverage" },
+                ],
+            },
+        ],
     ],
 };
